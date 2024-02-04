4108e25634b42baaf7ef7a0513b526462c436eb7
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-02-04 04:42:26 +0300

    finished with the game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Game = (() => {
   let gamePlayed = 0;
-
+  let msg = null;
   const GameBoard = (function () {
     const gameBoard = [];
     const rows = 3;
@@ -18,15 +18,15 @@ const Game = (() => {
   })();
 
   const Player = (function () {
-    const player1 = { name: "player", marker: "O" };
-    const player2 = { name: "computer", marker: "X" };
+    const player1 = { name: 'player', marker: 'O' };
+    const player2 = { name: 'computer', marker: 'X' };
     return { player1, player2 };
   })();
   let activePlayer = Player.player1;
   function PlayerTurn(row, column) {
     activePlayer = Player.player1;
     if (GameBoard.gameBoard[row][column] !== null) {
-      return "The position is taken";
+      return 'The position is taken';
     }
     if (GameBoard.gameBoard[row][column] == null) {
       GameBoard.gameBoard[row][column] = activePlayer.marker;
@@ -34,11 +34,7 @@ const Game = (() => {
     return;
   }
   const playRound = (row, column) => {
-    let msg;
     PlayerTurn(row, column);
-    msg = winCheck();
-
-    switchPlayerTurn();
 
     if (
       GameBoard.gameBoard.every((row) => {
@@ -50,11 +46,7 @@ const Game = (() => {
       return;
     }
 
-    computerChoice();
-    msg = winCheck();
-
     gamePlayed++;
-    return msg;
   };
   function switchPlayerTurn() {
     return activePlayer === Player.player1
@@ -76,95 +68,118 @@ const Game = (() => {
       }
     }
   }
+  function checkTie() {
+    if (gamePlayed === 4) {
+      console.log('its a tie');
+      gamePlayed = 0;
+      // GameBoard.gameBoard.forEach((element) => {
+      //   element.fill(null);
+      // });
 
-  function winCheck() {
-    let msg = null;
-    function checkTie() {
-      if (gamePlayed === 4) {
-        console.log("its a tie");
-        gamePlayed = 0;
-        // GameBoard.gameBoard.forEach((element) => {
-        //   element.fill(null);
-        // });
-        return "it's a tie";
-      }
+      return "it's a tie";
+    } else {
+      return null;
     }
-    function checkMarker() {
-      if (
-        (GameBoard.gameBoard[0][0] === activePlayer.marker &&
-          GameBoard.gameBoard[1][0] === activePlayer.marker &&
-          GameBoard.gameBoard[2][0] === activePlayer.marker) ||
-        (GameBoard.gameBoard[0][1] === activePlayer.marker &&
-          GameBoard.gameBoard[1][1] === activePlayer.marker &&
-          GameBoard.gameBoard[2][1] === activePlayer.marker) ||
-        (GameBoard.gameBoard[0][2] === activePlayer.marker &&
-          GameBoard.gameBoard[1][2] === activePlayer.marker &&
-          GameBoard.gameBoard[2][2] === activePlayer.marker) ||
-        (GameBoard.gameBoard[0][0] === activePlayer.marker &&
-          GameBoard.gameBoard[0][1] === activePlayer.marker &&
-          GameBoard.gameBoard[0][2] === activePlayer.marker) ||
-        (GameBoard.gameBoard[1][0] === activePlayer.marker &&
-          GameBoard.gameBoard[1][1] === activePlayer.marker &&
-          GameBoard.gameBoard[1][2] === activePlayer.marker) ||
-        (GameBoard.gameBoard[2][0] === activePlayer.marker &&
-          GameBoard.gameBoard[2][1] === activePlayer.marker &&
-          GameBoard.gameBoard[2][2] === activePlayer.marker) ||
-        (GameBoard.gameBoard[0][0] === activePlayer.marker &&
-          GameBoard.gameBoard[1][1] === activePlayer.marker &&
-          GameBoard.gameBoard[2][2] === activePlayer.marker) ||
-        (GameBoard.gameBoard[0][2] === activePlayer.marker &&
-          GameBoard.gameBoard[1][1] === activePlayer.marker &&
-          GameBoard.gameBoard[2][0] === activePlayer.marker)
-      ) {
-        if (activePlayer.marker === "X") {
-          msg = "Computer";
-        } else if (activePlayer.marker === "O") {
-          msg = "player";
-        }
-        gamePlayed = 0;
-        // GameBoard.gameBoard.forEach((element) => {
-        //   element.fill(null);
-        // });
-        return msg;
+  }
+  function checkMarker() {
+    if (
+      (GameBoard.gameBoard[0][0] === activePlayer.marker &&
+        GameBoard.gameBoard[1][0] === activePlayer.marker &&
+        GameBoard.gameBoard[2][0] === activePlayer.marker) ||
+      (GameBoard.gameBoard[0][1] === activePlayer.marker &&
+        GameBoard.gameBoard[1][1] === activePlayer.marker &&
+        GameBoard.gameBoard[2][1] === activePlayer.marker) ||
+      (GameBoard.gameBoard[0][2] === activePlayer.marker &&
+        GameBoard.gameBoard[1][2] === activePlayer.marker &&
+        GameBoard.gameBoard[2][2] === activePlayer.marker) ||
+      (GameBoard.gameBoard[0][0] === activePlayer.marker &&
+        GameBoard.gameBoard[0][1] === activePlayer.marker &&
+        GameBoard.gameBoard[0][2] === activePlayer.marker) ||
+      (GameBoard.gameBoard[1][0] === activePlayer.marker &&
+        GameBoard.gameBoard[1][1] === activePlayer.marker &&
+        GameBoard.gameBoard[1][2] === activePlayer.marker) ||
+      (GameBoard.gameBoard[2][0] === activePlayer.marker &&
+        GameBoard.gameBoard[2][1] === activePlayer.marker &&
+        GameBoard.gameBoard[2][2] === activePlayer.marker) ||
+      (GameBoard.gameBoard[0][0] === activePlayer.marker &&
+        GameBoard.gameBoard[1][1] === activePlayer.marker &&
+        GameBoard.gameBoard[2][2] === activePlayer.marker) ||
+      (GameBoard.gameBoard[0][2] === activePlayer.marker &&
+        GameBoard.gameBoard[1][1] === activePlayer.marker &&
+        GameBoard.gameBoard[2][0] === activePlayer.marker)
+    ) {
+      if (activePlayer.marker === 'X') {
+        return (msg = 'Computerc has won');
+      } else if (activePlayer.marker === 'O') {
+        return (msg = 'player has won');
+      } else {
+        return null;
       }
+      gamePlayed = 0;
+      // GameBoard.gameBoard.forEach((element) => {
+      //   element.fill(null);
+      // });
     }
-    checkTie();
-    checkMarker();
-    return msg;
+  }
+
+  function resetGame() {
+    GameBoard.gameBoard.forEach((element) => {
+      element.fill(null);
+    });
+    gamePlayed = 0;
   }
 
   return {
-    winCheck,
     activePlayer,
     playRound,
     computerChoice,
     switchPlayerTurn,
     PlayerTurn,
     GameBoard,
+    resetGame,
+    checkMarker,
+    checkTie,
   };
 })();
 
 function renderGame() {
-  const btns = document.querySelectorAll(".block");
-  const winner = document.querySelector(".winner");
+  const btns = document.querySelectorAll('.block');
+  const winner = document.querySelector('.winner');
   let column, row;
   let winner_;
   btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener('click', () => {
       column = btn.dataset.column;
       row = btn.dataset.row;
-      // Game.PlayerTurn(row, column);
-      winner_ = Game.playRound(row, column);
-      console.log(Game.GameBoard.gameBoard);
 
-      // winner.textContent = Game.winCheck();
-      // Game.switchPlayerTurn();
-      // Game.computerChoice();
+      Game.playRound(row, column);
+      winner_ = Game.checkMarker() || Game.checkTie();
+      if (winner_ !== null) {
+        winner.textContent = `${winner_} has won!`;
+        return;
+      }
+      Game.switchPlayerTurn();
+      Game.computerChoice();
+      winner_ = Game.checkMarker() ?? Game.checkTie();
+      console.log(winner_);
       btns.forEach((btn) => {
         btn.textContent =
           Game.GameBoard.gameBoard[btn.dataset.row][btn.dataset.column];
       });
-      winner.textContent = winner_;
+
+      if (winner_ !== null) {
+        winner.textContent = winner_;
+        return;
+      }
+    });
+  });
+  const reset = document.querySelector('.reset');
+  console.log(reset);
+  reset.addEventListener('click', () => {
+    Game.resetGame();
+    btns.forEach((btn) => {
+      btn.textContent =
+        Game.GameBoard.gameBoard[btn.dataset.row][btn.dataset.column];
     });
   });
 }
